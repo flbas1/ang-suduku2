3e35854821ec1c21ece1f7482c97f8aca896cf04
Chris Stewart <flbas1@gmail.com>
2018-07-07 16:04:56 -0400

    deployable pwa

---
+++ src/manifest.js
@@ -0,0 +1,15 @@
+{
+    "name": "Championship Sudoku",
+    "short_name": "Championship Sudoku",
+    "icons": [{
+      "src": "/images/logo.png",
+        "sizes": "128x128",
+        "type": "image/png"
+      }
+      
+    ],
+    "start_url": "/index.html",
+    "display": "standalone",
+    "background_color": "#3E4EB8",
+    "theme_color": "#2F3BA2"
+  }
