c790f20f5ccc15922b5ce1bd6a1f1c92985eb528
Chris Stewart <flbas1@gmail.com>
2018-07-08 11:22:14 -0400

    adding service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { GridComponent } from './grid/grid.component';
     GridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
